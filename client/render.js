e3e1b6164fd2b080db59d1e4264deafc196d9bee
Aaron Zhang <a.zhang1997@outlook.com>
2020-11-16 14:22:25 -0500

    updated render.js and added login folder

--- client/render.js
+++ client/render.js
@@ -1,12 +1,28 @@
-export async function loadIntoDOM() {
-    const $root = $('#root');
-    let test = await axios({
-        method: 'get',
-        url: 'http://localhost:5000/api/apiTest'
-    })
-    $root.append(`<p>${test.data.body}</p>`) 
+export const renderHeader = function () {};
+
+export const renderNavBar = function () {};
+
+export const renderBody = function () {
+  //call renderLeaderboard
+  //call renderAbout
+  //call renderTwitterFeed
 };
 
+export const renderLeaderboard = function () {};
+
+export const renderAbout = function () {};
+
+export const renderTwitterFeed = function () {};
+
+export async function loadIntoDOM() {
+  const $root = $("#root");
+  let test = await axios({
+    method: "get",
+    url: "http://localhost:5000/api/apiTest",
+  });
+  $root.append(`<p>${test.data.body}</p>`);
+}
+
 $(function () {
-    loadIntoDOM();
+  loadIntoDOM();
 });
