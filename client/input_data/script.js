f5b3b75d12ede66d277f2add7db45ada1950547b
klu13 <klu13@ad.unc.edu>
2020-11-16 18:49:42 -0500

    toggle dropdown and indentation format

--- client/input_data/script.js
+++ client/input_data/script.js
@@ -1,13 +1,14 @@
 
 
-// export const loadIntoDOM = function() {
-//     const $root = $("#root");
+export const loadIntoDOM = function() {
+    const $root = $("#root");
+    $(document).on('click', '.dropdown', function(event) {
+        event.stopPropagation()
+        $('.dropdown').toggleClass('is-active')
+    })
 
+}
 
-// }
-
-
-
-// $(function () {
-//     loadIntoDOM();
-// })
+$(function () {
+    loadIntoDOM();
+})
