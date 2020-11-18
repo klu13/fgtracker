3a30714d4eebedb8d251f8d4d846aa71cd23f356
klu13 <klu13@ad.unc.edu>
2020-11-17 22:50:04 -0500

    setting up cloud firestore on backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config()
 
 // Setting up the main server app
 const app = express();
