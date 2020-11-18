68bbaa66fe029eb97d9234ae3e427bb9ccb3a4a2
Aaron Zhang <a.zhang1997@outlook.com>
2020-11-18 17:38:25 -0500

    startedNavBarLoginObserver

--- client/render.js
+++ client/render.js
@@ -1,3 +1,103 @@
+export const renderNavbar = function () {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      // const db = firebase.firestore();
+      // var docRef = db.collection("cities").doc("SF");
+
+      // docRef
+      //   .get()
+      //   .then(function (doc) {
+      //     if (doc.exists) {
+      //       console.log("Document data:", doc.data());
+      //     } else {
+      //       // doc.data() will be undefined in this case
+      //       console.log("No such document!");
+      //     }
+      //   })
+      //   .catch(function (error) {
+      //     console.log("Error getting document:", error);
+      //   });
+
+      let html = `
+      <nav class="navbar is-transparent" role="navigation" aria-label="main navigation" style="background-color: #add8e6;">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="index.html">
+            <h1 class="title is-1" style="color: #e75480;">FALL GUYS STATS</h1>
+          </a>
+        </div>
+    
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="index.html">
+              Home
+            </a>
+    
+            <a class="navbar-item" href="input_data/index.html">
+              Enter Stats
+            </a>
+
+            <a class="navbar-item">
+              Career Profile
+            </a>
+          </div>
+    
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <p></p>
+          </div>
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button" style="background-color: #e75480;" href="login/index.html">
+                <p>Login/Sign up</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>`;
+      return html;
+    } else {
+      // No user is signed in.
+      let html = `
+      <nav class="navbar is-transparent" role="navigation" aria-label="main navigation" style="background-color: #add8e6;">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="index.html">
+            <h1 class="title is-1" style="color: #e75480;">FALL GUYS STATS</h1>
+          </a>
+        </div>
+    
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="index.html">
+              Home
+            </a>
+    
+            <a class="navbar-item" href="input_data/index.html">
+              Enter Stats
+            </a>
+
+            <a class="navbar-item">
+              Career Profile
+            </a>
+          </div>
+    
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button" style="background-color: #e75480;" href="login/index.html">
+                <p>Login/Sign up</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>`;
+      return html;
+    }
+  });
+};
+
 export const renderBody = function () {
   let html = `
     <section class="section">
