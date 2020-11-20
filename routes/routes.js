02122d37a1315e4de1bf85a5ee0d65e6ceaabf5f
klu13 <klu13@ad.unc.edu>
2020-11-20 15:52:33 -0500

    added undo button

--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,6 @@ router.get('/leaderboard', controllers.leaderboard)
 router.post('/saveRound', controllers.saveRound)
 router.put('/updateUser', controllers.updateUser)
 router.get('/getUser', controllers.getUser)
+router.delete('/undo', controllers.undo)
 
 module.exports = router;
